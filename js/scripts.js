2235aa1c995ccf2d8087eed1c28870eec7bc7b0d
John Rock <46692803+john-rock@users.noreply.github.com>
2019-03-17 21:35:54 -0400

    bind data from selected driver to individual span

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ console.clear()
 
 Vue.config.devtools = true;
 
-var url = "https://spreadsheets.google.com/feeds/list/1oBMFnukoAvh8CtqHFCr04uOxeDckdHgHlHbOXbaWb04/od6/public/values?alt=json"
+var url = "https://spreadsheets.google.com/feeds/list/1Kro7GozZoMHv0j_iQUqZLmuN9dONEYBWRb9grtneiQs/od6/public/values?alt=json"
 
 let app = new Vue({
   el: '#app',
@@ -11,24 +11,26 @@ let app = new Vue({
   },
   
   data: {
-    selected: null,
-    selected2: null,
-    drivers: []
+    selected: '',
+    selected2: '',
+    driverList: []
   },
 
   computed: {
     orderedDrivers: function () {
-      return _.orderBy(this.drivers, 'gsx$driver.$t')
+      return _.orderBy(this.driverList, 'gsx$driver.$t')
     }
   },
   
   methods:{
-    getDrivers: function(){
-      axios.get(url)
-        .then(results => {
-        this.drivers = results.data.feed.entry;
-        console.log(this.drivers);
+    getDrivers: async function(){
+      this.status = "Loading...";
+      await axios.get(url)
+        .then((results) => {
+        this.driverList= results.data.feed.entry;
+        console.log(this.driverList);
         console.log(url);
+
       })
       .catch (function (error){
         console.log(error)
